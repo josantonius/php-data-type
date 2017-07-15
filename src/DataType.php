cc0c8eff7d65cd1002869866acfc4b6e1a5b89e0
Josantonius <info@josantonius.com>
2017-07-16 01:21:23 +0200

    Updated to 1.1.2 version

--- src/DataType.php
+++ src/DataType.php
@@ -3,15 +3,13 @@
  * PHP simple library for managing of data types.
  * 
  * @author     Josantonius - hello@josantonius.com
- * @copyright  Copyright (c) 2016
+ * @copyright  Copyright (c) 2016 - 2017
  * @license    https://opensource.org/licenses/MIT - The MIT License (MIT)
  * @link       https://github.com/Josantonius/PHP-DataType
  * @since      1.0.0
  */
 
 namespace Josantonius\DataType;
-
-# use Josantonius\DataType\Exception\DataTypeException;
 
 /**
  * Data type handler.
